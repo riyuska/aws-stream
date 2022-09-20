5345c8668165741479549ae66dc1f71e86246d68
Your Namecls <you@example.com>
2022-09-20 14:43:15 +0700

    Delete Movie

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -117,4 +117,10 @@ class MovieController extends Controller
 
         return redirect()->route('admin.movie')->with('success', 'Movie Update');
     }
+
+    public function destroy($id)
+    {
+        Movie::find($id)->delete();
+        return redirect()->route('admin.movie')->with('success', 'Movie Delete'); 
+    }
 }
