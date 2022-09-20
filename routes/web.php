7e97b5ea83bfe55339fbf3525cf0870ac67c12b5
Your Namecls <you@example.com>
2022-09-20 17:06:22 +0700

    Fix Transaction

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\MovieController;
+use App\Http\Controllers\Admin\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,6 +21,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin'], function (){
     Route::view('/', 'admin.dashboard')->name('admin.dashboard');
+
+    Route::get('transaction', [TransactionController::class, 'index'])->name('admin.transaction');
 
     Route::group(['prefix' => 'movie'], function(){
         Route::get('/', [MovieController::class, 'index'])->name('admin.movie');
