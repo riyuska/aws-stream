ba243542336e511ebde065b1cd5dbb85ab0defae
Your Namecls <you@example.com>
2022-09-22 15:40:28 +0700

    Integrasi Midtrans 2

--- app/Http/Controllers/Member/TransactionController.php
+++ app/Http/Controllers/Member/TransactionController.php
@@ -3,13 +3,49 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
+use App\Models\Package;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class TransactionController extends Controller
 {
     public function store(Request $request)
     {
-        dd($request->all());
-        return "test";
+        $package = Package::find($request->package_id);
+
+        $customer = auth()->user();
+
+        $transaction = Transaction::create([
+            'package_id' => $package->id,
+            'user_id' => $customer->id,
+            'amount' => $package->price,
+            'transaction_code' => strtoupper(Str::random(10)),
+            'status' => 'pending'
+        ]);
+
+        //mENgambil data custamer atau data orang mengambil paket
+
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        \Midtrans\Config::$isProduction = (bool) env('MIDTRANS_IS_PRODUCTION');
+        \Midtrans\Config::$isSanitized = (bool) env('MIDTRANS_IS_SANITIZED');
+        \Midtrans\Config::$is3ds = (bool) env('MIDTRANS_IS_3DS');
+        
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => $transaction->transaction_code,
+                'gross_amount' => $transaction->amount,
+            ),
+            'customer_details' => array(
+                'first_name' => $customer->name,
+                'last_name' => $customer->name,
+                'email' => $customer->email,
+            ),
+        );
+
+        $createMidtransTransaction = \Midtrans\Snap::createTransaction($params);
+        $midtransRedirectUrl = $createMidtransTransaction->redirect_url;
+
+        return redirect($midtransRedirectUrl);
     }
 }
