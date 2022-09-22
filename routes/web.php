41641ded006dd8ca40f9af5035600c51726c46e6
Your Namecls <you@example.com>
2022-09-22 21:29:12 +0700

    Subscription Page 1

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Member\MovieController as MemberMovieController;
 use App\Http\Controllers\Member\PricingController;
 use App\Http\Controllers\Member\RegisterController;
 use App\Http\Controllers\Member\TransactionController as MemberTransactionController;
+use App\Http\Controllers\Member\UserPremiumController;
 
 /*
 |--------------------------------------------------------------------------
@@ -72,6 +73,8 @@ Route::group(['prefix' => 'member', 'middleware' => ['auth']], function() {
     Route::get('/logout', [MemberLoginController::class, 'logout'])->name('member.logout');
 
     Route::post('transaction', [MemberTransactionController::class, 'store'])->name('member.transaction.store');
+
+    Route::get('subscription', [UserPremiumController::class, 'index'])->name('member.user_premium.index');
 
     Route::get('movie{id}', [MemberMovieController::class, 'show'])->name('member.movie.detail');
 });
